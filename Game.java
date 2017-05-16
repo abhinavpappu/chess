f9dd00d0aecf33bbf6a5018a73877a82eadd9afb
Abhinav Pappu <apappu9@students.d125.org>
2017-05-15 21:23:15 -0500

    Converted part of applet to JFrame. Still need to finish.

--- Game.java
+++ Game.java
@@ -1,4 +1,6 @@
-
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
 /**
  * Write a description of class Game here.
  * 
@@ -8,7 +10,43 @@
 public class Game
 {
     public static void main(String[] args){
-        GUI gui = new GUI();
-        gui.setSize(500, 500);
+        Color boardWhite = new Color(176, 190, 197), boardBlack = new Color(84, 110, 122);
+        JFrame frame = new JFrame("Chess");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(500, 500);
+        frame.setLayout(new GridLayout(8, 8));
+        Board board = new Board(true);
+        JPanel[][] squares = new JPanel[8][8];
+        JLabel[][] labels = new JLabel[8][8];
+        for(int i = 0; i < 8; i++){
+            for(int j = 0; j < 8; j++){
+                squares[i][j] = new JPanel();
+                squares[i][j].setBackground((i % 2 == j % 2)? boardWhite : boardBlack);
+                squares[i][j].setLayout(new GridBagLayout());
+                String icon = "";
+                Color color = (i % 2 == j % 2)? boardWhite : boardBlack;
+                if(board.getPieces()[i][j] != null){
+                    icon = board.getPieces()[i][j].getIcon();
+                    color = board.getPieces()[i][j].getColor()? Color.WHITE : Color.BLACK;
+                }
+                labels[i][j] = new JLabel(icon);
+                labels[i][j].setFont(new Font("TimesRoman", Font.PLAIN, (int)(frame.getHeight() * 1.0/12)));
+                labels[i][j].setForeground(color);
+                squares[i][j].add(labels[i][j], new GridBagConstraints());
+                frame.add(squares[i][j]);
+            }
+        }
+        
+        frame.addComponentListener(new ComponentAdapter(){
+            public void componentResized(ComponentEvent evt){
+                JFrame frame = (JFrame)evt.getSource();
+                for(int i = 0; i < 64; i++){
+                    JLabel label = (JLabel)((JPanel)frame.getRootPane().getComponent(i)).getComponent(0);
+                    label.setFont(new Font("TimesRoman", Font.PLAIN, (int)(frame.getHeight() * 1.0/12)));
+                }
+            }
+        });
+        
+        frame.setVisible(true);
     }
 }
